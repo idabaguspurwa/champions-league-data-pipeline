54cdddde1dd0896e20bd66ea1d2ab68863521265
idabaguspurwa <ida.bagus862@gmail.com>
2025-07-17 06:50:30 +0700

    adding libraries

--- airflow_dags/champions_pipeline_dag.py
+++ airflow_dags/champions_pipeline_dag.py
@@ -68,7 +68,7 @@ def create_kubernetes_pod_operator(
 
 @dag(
     dag_id='champions_league_pipeline_v2',
-    start_date=pendulum.now(tz="Asia/Jakarta").subtract(days=1),
+    start_date=pendulum.datetime(2025, 7, 16, tz="Asia/Jakarta"),
     schedule='0 */6 * * *',  # Every 6 hours
     catchup=False,
     max_active_runs=1,
