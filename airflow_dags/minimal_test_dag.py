79b9e239582be3367685c64ebc72d1b85d84a91c
idabaguspurwa <ida.bagus862@gmail.com>
2025-07-18 18:35:23 +0700

    fix: minimal_test_dag

--- airflow_dags/minimal_test_dag.py
+++ airflow_dags/minimal_test_dag.py
@@ -2,11 +2,17 @@ import pendulum
 from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 
+# Define default arguments, including a specific owner
+default_args = {
+    'owner': 'data-engineering-team'
+}
+
 @dag(
     dag_id='minimal_test_dag',
-    start_date=pendulum.datetime(2025, 7, 17, tz="UTC"),
+    start_date=pendulum.datetime(2025, 7, 18, tz="Asia/Jakarta"),
     schedule=None,
     catchup=False,
+    default_args=default_args, # Apply the default args to the DAG
     tags=['test'],
 )
 def minimal_test_dag():
